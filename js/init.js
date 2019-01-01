cfe6a5fcbbbe662ac487fe96bd7777fc3a2c23be
Aurelio Greco <aureliogreco@outlook.it>
2019-01-01 18:49:17 +0100

    Changed menu mobile.

--- js/init.js
+++ js/init.js
@@ -1,6 +1,4 @@
-(function($){
-  $(function(){
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+$(document).ready(function(){
+	$('.sidenav').sidenav();
+	$('.parallax').parallax();
+}); // end of document ready
